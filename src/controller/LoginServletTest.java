352a5d7bc37d5096a2e463cdf3b678dd10baca89
EXCITE-2003-03\2020 <shota.katahira@excite-software.co.jp>
2021-05-18 11:52:09 +0900

    loginservletテスト userDB起動

--- src/controller/LoginServletTest.java
+++ src/controller/LoginServletTest.java
@@ -1,9 +1,47 @@
 package controller;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import javax.servlet.http.HttpSession;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import dbmodel.UserDB;
+import model.User;
+
 public class LoginServletTest {
 
 	private LoginServlet login;
-	
-	@
-	
+	private UserDB udb;
+
+	@BeforeEach
+	public void name() {
+		login = new LoginServlet();
+		udb = new UserDB();
+	}
+
+	@Test
+	public void doGetTest() {
+
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		HttpSession session = request.getSession();
+		User user = udb.getUser("1", "p");
+		session.setAttribute("user", user);
+
+		try {
+			login.doGet(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		assertEquals("user", nullValue(), session.getAttribute("user"));
+		assertEquals("message", "ログアウトしました", request.getAttribute("message"));
+
+	}
+
 }
