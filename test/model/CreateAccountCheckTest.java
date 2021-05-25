90835ab9487b9f352a938bfa802a22f3504ff8e6
EXCITE-2003-03\2020 <shota.katahira@excite-software.co.jp>
2021-05-25 10:49:20 +0900

    CreateAccountCheckTest インポート変更

--- test/model/CreateAccountCheckTest.java
+++ test/model/CreateAccountCheckTest.java
@@ -1,7 +1,7 @@
 package model;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
