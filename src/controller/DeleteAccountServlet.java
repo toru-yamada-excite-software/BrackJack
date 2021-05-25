3b07ebdf1597ce1c9b65fe09d4cc00c2a8a28e43
EXCITE-2003-03\2020 <shota.katahira@excite-software.co.jp>
2021-05-25 14:40:01 +0900

    DeleteAccountServletパス追加

--- src/controller/DeleteAccountServlet.java
+++ src/controller/DeleteAccountServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpSession;
 import dbmodel.UserDB;
 import model.User;
 
+@WebServlet("/DeleteAccountServlet")
 public class DeleteAccountServlet extends HttpServlet {
 	private static final long serialVersionUID = -1368986875981582268L;
 
---
+++ src/model/Card.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Card {
+
+	private int number;
+	private String suit;
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+}
