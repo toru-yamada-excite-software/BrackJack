84a84212f3d64bea0c140814c6b97bfbef4767ad
EXCITE-2003-03\2020 <shota.katahira@excite-software.co.jp>
2021-05-31 09:08:22 +0900

    GameManagerに処理移動

--- src/model/GameManager.java
+++ src/model/GameManager.java
@@ -1,7 +1,13 @@
 package model;
 
+import java.sql.Timestamp;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import dbmodel.GameDB;
+import dbmodel.UserDB;
 
 public class GameManager {
 
@@ -15,6 +21,32 @@ public class GameManager {
 		int command = Integer.parseInt(request.getParameter("command"));
 
 		if (command == 0) {
+
+			deck = player.draw(deck);
+
+			if (player.getBust()) {
+				session.setAttribute("message", "Lose");
+				session.setAttribute("player", player);
+				Timestamp playTime = new Timestamp(System.currentTimeMillis());
+				Game game = new Game();
+				game.setUserId(user.getId());
+				game.setWinLose(2);
+				game.setPlayTime(playTime);
+				GameDB gdb = new GameDB();
+				gdb.insertGame(game);
+				user.setPlay(user.getPlay() + 1);
+				user.setWinRate((double) user.getWin() / user.getPlay());
+				UserDB udb = new UserDB();
+				udb.updateUserRecord(user);
+				session.setAttribute("user", user);
+				RequestDispatcher rd = request.getRequestDispatcher("menu.jsp");
+				rd.forward(request, response);
+			} else {
+				session.setAttribute("player", player);
+				session.setAttribute("deckInf", deckInf);
+				RequestDispatcher rd = request.getRequestDispatcher("menu.jsp");
+				rd.forward(request, response);
+			}
 
 		}
 
