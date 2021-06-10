b88b42cc876a5b1a71112cf14954c3dc11d586b9
EXCITE-2003-03\2020 <shota.katahira@excite-software.co.jp>
2021-06-10 16:43:55 +0900

    Player#judgeSplit 追加

--- src/model/Player.java
+++ src/model/Player.java
@@ -3,6 +3,8 @@ package model;
 public class Player extends PlayerBase {
 	private static final long serialVersionUID = -1993110647316686215L;
 
+	private boolean split = false;
+
 	@Override
 	public void draw(Deck deck) {
 
@@ -17,6 +19,19 @@ public class Player extends PlayerBase {
 			drawBase(deck);
 		}
 
+		judgeSplit();
+	}
+
+	public void judgeSplit() {
+
+		if (hand.get(0).getNumber() == hand.get(1).getNumber()) {
+			split = true;
+		}
+
+	}
+
+	public boolean getSplit() {
+		return split;
 	}
 
 }
