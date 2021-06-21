80964d72ef79eb2f51b4c410f97eaa6b68fbc58a
EXCITE-2003-03\2020 <shota.katahira@excite-software.co.jp>
2021-06-21 15:41:32 +0900

    udgeGameEndTest 作成

--- test/controller/SplitServletTest.java
+++ test/controller/SplitServletTest.java
@@ -3,59 +3,31 @@ package controller;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpSession;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import model.Card;
 import model.Deck;
 import model.GameInf;
-import model.Hand;
 import model.Player;
 
 public class SplitServletTest {
 
-	@InjectMocks
-	private SplitServlet ss = new SplitServlet();
-
-	@Mock
-	private GameInf gi;
-
-	@Mock
-	private Player player;
-
-	private Hand hand;
+	private SplitServlet ss;
 	private Deck decks;
 	private LinkedList<Card> deck = new LinkedList<Card>();
 
-	@Mock
-	private ArrayList<Hand> handList;
-
-	@Mock
-	private LinkedList<Card> handCard;
-
-	@Mock
-	private MockHttpServletRequest request;
-
-	@Mock
-	private RequestDispatcher rd;
-
 	@BeforeEach
 	public void setup() {
-		MockitoAnnotations.initMocks(this);
 
+		ss = new SplitServlet();
 		decks = new Deck();
-		hand = new Hand();
 
 		for (int j = 1; j < 14; j++) {
 
---
+++ test/model/JudgeGameEndTest.java
@@ -0,0 +1,60 @@
+package model;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import java.util.LinkedList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class JudgeGameEndTest {
+
+	private Player player;
+	private Deck decks;
+	private LinkedList<Card> deck = new LinkedList<Card>();
+	private Split split;
+	private GameInf gi;
+	private Hit hit;
+
+	@BeforeEach
+	public void setup() {
+
+		player = new Player(100);
+		decks = new Deck();
+		hit = new Hit();
+
+		for (int j = 1; j <= 13; j++) {
+
+			for (int i = 0; i < 4; i++) {
+
+				Card card = new Card(i, j);
+				deck.add(card);
+			}
+
+		}
+
+		decks.setDeck(deck);
+		player.firstDraw(decks, 1);
+		gi = new GameInf(player, null, decks);
+		split = new Split();
+		gi = split.doSplit(gi);
+
+		for (int i = 0; i < 8; i++) {
+			gi = hit.doHit(gi, 0);
+		}
+
+	}
+
+	@Test
+	public void judgeTest() {
+
+		int expected = 1;
+
+		int actual = JudgeGameEnd.judge(gi.getPlayer());
+
+		assertThat(actual, is(expected));
+
+	}
+
+}
