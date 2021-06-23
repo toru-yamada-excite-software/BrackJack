b63ccc9c4027085c5106b730f420b3cf2b30761f
EXCITE-2003-03\2020 <shota.katahira@excite-software.co.jp>
2021-06-23 18:05:05 +0900

    デモ用コンストラクタ作成

--- src/model/Deck.java
+++ src/model/Deck.java
@@ -11,9 +11,10 @@ public class Deck implements Serializable {
 
 	public Deck() {
 
-		for (int i = 0; i < 4; i++) {
+		for (int j = 1; j <= 13; j++) {
 
-			for (int j = 1; j <= 13; j++) {
+			for (int i = 0; i < 4; i++) {
+
 				Card card = new Card(i, j);
 				deck.add(card);
 			}
@@ -21,6 +22,24 @@ public class Deck implements Serializable {
 		}
 		Collections.shuffle(deck);
 	}
+
+	//	public Deck() {
+	//
+	//		for (int j = 1; j <= 12; j++) {
+	//
+	//			for (int i = 0; i < 4; i++) {
+	//
+	//				Card card = new Card(i, j);
+	//				deck.add(card);
+	//			}
+	//
+	//		}
+	//		deck.add(new Card(2, 13));
+	//		deck.add(new Card(3, 13));
+	//		Collections.shuffle(deck);
+	//		deck.set(0, new Card(0, 13));
+	//		deck.set(1, new Card(1, 13));
+	//	}
 
 	public void setDeck(LinkedList<Card> deck) {
 		this.deck = deck;
