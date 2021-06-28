be3ecb208df413fdeee7134bdfeaa7e73af2061a
EXCITE-2003-03\2020 <shota.katahira@excite-software.co.jp>
2021-06-28 10:19:46 +0900

    enum Number 作成

--- src/model/Card.java
+++ src/model/Card.java
@@ -6,90 +6,107 @@ public class Card implements Serializable {
 	private static final long serialVersionUID = 3314780953415621490L;
 
 	//0:スペード、1:クラブ、2:ダイヤ、3:ハート
-	private int suite;
-	private int number;
+	private Suite suite;
+	private Number number;
 
 	public Card(int suite, int number) {
-		this.suite = suite;
-		this.number = number;
+		this.suite = Suite.getBySuiteId(suite);
+		this.number = Number.getByNumberId(number);
 	}
 
 	public enum Suite {
 
 		SPADE("♠", 0), CLUB("♣", 1), DIAMOND("♢", 2), HEART("♡", 3);
 
-		private String label;
-		private int id;
+		private String suiteMark;
+		private int suiteId;
 
-		private Suite(String label, int id) {
-			this.label = label;
-			this.id = id;
+		private Suite(String label, int suiteId) {
+			this.suiteMark = label;
+			this.suiteId = suiteId;
 		}
 
-		public String getLabel() {
-			return label;
+		public String getSuiteMark() {
+			return suiteMark;
 		}
 
-		public int getId() {
-			return id;
+		public int getSuiteId() {
+			return suiteId;
 		}
 
-		public static Suite getById(int id) {
+		public static Suite getBySuiteId(int suiteId) {
 
 			for (Suite suite : Suite.values()) {
-				if (suite.getId() == id) {
+				if (suite.getSuiteId() == suiteId) {
 					return suite;
 				}
 			}
+
 			return null;
 		}
 	}
 
-	public String getFaceCard() {
+	public enum Number {
 
-		switch (number) {
-		case 1:
-			return "A";
+		ONE(1, "A"), TWO(2, "2"), THREE(3, "3"), FOUR(4, "4"), FIVE(5, "5"), SIX(6, "6"), SEVEN(7, "7"), EIGHT(8,
+				"8"), NINE(9, "9"), TEN(10, "10"), ELEVEN(11, "J"), TWELVE(12, "Q"), THIRTEEN(13, "K");
 
-		case 11:
-			return "J";
+		private int number;
+		private String face;
 
-		case 12:
-			return "Q";
+		private Number(int numberId, String face) {
+			this.number = numberId;
+			this.face = face;
+		}
 
-		case 13:
-			return "K";
+		public int getNumber() {
+			return number;
+		}
 
-		default:
-			return Integer.toString(number);
+		public String getFace() {
+			return face;
+		}
+
+		public static Number getByNumberId(int numberId) {
+
+			for (Number number : Number.values()) {
+				if (number.getNumber() == numberId) {
+					return number;
+				}
+			}
+
+			return null;
 		}
 
 	}
 
 	public int getNumber() {
-		return number;
+		return number.getNumber();
 	}
 
 	public int getCardScore() {
 
-		if (number > 10) {
+		if (number.getNumber() > 10) {
 			return 10;
 		}
 
-		return number;
+		return number.getNumber();
 	}
 
 	public int getAcardScore() {
 
-		if (number == 1) {
+		if (number.getNumber() == 1) {
 			return 11;
 		}
 
 		return getCardScore();
 	}
 
+	public String getFaceCard() {
+		return number.getFace();
+	}
+
 	public String getSuite() {
-		Suite suite = Suite.getById(this.suite);
-		return suite.getLabel();
+		return suite.getSuiteMark();
 	}
 }
